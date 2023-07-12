e48efe24de7054b3890c7eb2fb4703ba6a679bc9
seoyeon <wjddowkd456@naver.com>
2023-07-12 23:21:25 +0900

    feat: 라이트모드, 다크모드 색상 지정

---
+++ src/style/theme.js
@@ -0,0 +1,34 @@
+export const lightTheme = {
+  bgColor: "#ffffff",
+  mainColor: "#1A374D",
+  subColor: "#406882",
+  accentColor: "#6998AB",
+  lightColor: "#B1D0E0",
+  fontColor: "#767676",
+  highlightFont: "#000000",
+  colorFont: "#1A374D",
+  disabledButton: "#B1D0E0",
+  button: "#406882",
+  border: "#f2f2f2",
+};
+
+export const darkTheme = {
+  bgColor: "#212426",
+  mainColor: "#212426",
+  subColor: "#B1D0E0",
+  accentColor: "#6998AB",
+  lightColor: "#406882",
+  fontColor: "#c7c7c7",
+  highlightFont: "#ffffff",
+  colorFont: "#B1D0E0",
+  disabledButton: "#363f45",
+  button: "#406882",
+  border: "#363f45",
+};
+
+export const theme = {
+  lightTheme,
+  darkTheme,
+};
+
+export default theme;
